dba5e1793d420839fd589a3e42d6800e52607d52
Sean Quillen <sean.r.quillen@gmail.com>
2021-01-12 17:16:44 -0600

    make port dynamic

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 const { MongoClient } = require('mongodb');
 import { AddressInfo } from 'net'
-import { app } from '../server/index';
+import { app } from '../server';
 import RestaurantsDAO from './dao/RestaurantsDAO';
 const mongoURI = process.env.DB_URI || 'mongodb://localhost:27017/atxhh-dev';
-
+const appPort = (process.env.PORT || 5000);
 const client = new MongoClient(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -21,14 +21,12 @@ async function connect() {
       RestaurantsDAO.injectDB(client),
     ];
     await Promise.all(connections);
-    const server = app.listen(5000, '0.0.0.0', () => {
+    const server = app.listen(appPort as number, '0.0.0.0', () => {
       const {port, address} = server.address() as AddressInfo;
       console.log(`Server listening on: http://${address}:${port}`);
   });
   } catch (e) {
     console.error({ message: e.stack });
-  } finally {
-    // await client.close();
   }
 }
 
